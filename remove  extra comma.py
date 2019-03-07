916f5ff3f54567d0ccb35ff543c94a8937449527
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2019-03-07 20:30:55 +0500

    Update remove  extra comma.py

--- remove  extra comma.py
+++ remove  extra comma.py
@@ -1,6 +1,6 @@
 with open(r"C:\\Users\\haieer\\Documents\\DevC++ codes\\1stWeb & E-commerce.txt","r+")as input:
 
-    a = input.read()
+   File  = input.read()
     with open(r"C:\\Users\\haieer\\Desktop\2ndWeb & E-commerce.txt", "w+")as input:
-     input.writelines(str(a).replace(",","").replace("Pakistan",",").replace("PKR",",PKR").replace("USD",",USD").replace("ALL",",ALL").replace("GBP",",GBP").replace("EUR",",EUR").replace("AED",",AED").replace("PAB",",PAB").replace("SAR",",SAR").replace("AFN",",AFN").replace("CAD",",CAD").replace("JPY",",JPY"))
+     input.writelines(str(File).replace(",","").replace("Pakistan",",").replace("PKR",",PKR").replace("USD",",USD").replace("ALL",",ALL").replace("GBP",",GBP").replace("EUR",",EUR").replace("AED",",AED").replace("PAB",",PAB").replace("SAR",",SAR").replace("AFN",",AFN").replace("CAD",",CAD").replace("JPY",",JPY"))
     input.close
